76b8d149d85145c2e87fa6a00dd35524c3b934e0
Lights0n <lyadouskiy@gmail.com>
2022-10-12 22:57:12 +0300

    feat: carbon implemented

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(activity) {
+  // console.log(typeof (activity) == 'string');
+  // console.log(typeof (+activity));
+
+  if ((typeof (activity) == 'string' && typeof (+activity) == 'number' && +activity > 0 && +activity < 15)) {
+    const MODERN_ACTIVITY = 15;
+    const HALF_LIFE_PERIOD = 5730;
+    let k = 0.693 / HALF_LIFE_PERIOD
+    let answer = (Math.log(MODERN_ACTIVITY / +activity)) / k
+    answer = Math.ceil(answer)
+    return answer
+  }
+  else {
+    return false
+  }
+
 }
 
 module.exports = {
   dateSample
 };
+
+// console.log(module.exports.dateSample('0.1'))
