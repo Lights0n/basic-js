f2348d41917567e8feedd09f9660fd2c08b61134
Lights0n <lyadouskiy@gmail.com>
2022-10-13 01:55:08 +0300

    feat: implement delete-digit solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(str1, str2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+
+  let str1Arr = [...str1]
+  let str2Arr =[...str2]
+
+  // console.log(str1Arr, 'arr');
+  let max = Math.max(str1Arr.length, str2Arr.length)
+  // console.log(str2Arr, 'arr');
+
+let res = 0
+  for (let i = 0; i < max; i++) {
+    if (str2Arr.includes(str1Arr[i])){
+      // это пиздец, а не ЯП, отвечаю. почему нельзя отнять массивы просто ебучие
+      str2Arr[str2Arr.indexOf(str1Arr[i])] = ''
+      res++
+      continue
+    }
+  }
+
+  return res
 }
 
 module.exports = {
   getCommonCharacterCount
 };
+
+// console.log(module.exports.getCommonCharacterCount('aabcc', 'adcaa'))
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -3,19 +3,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
- *
  * @param {Number} n
  * @return {Number}
- *
  * @example
  * For n = 152, the output should be 52
- *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(number2) {
+  let number = Array.from(number2.toString())
+  // console.log(number);
+  let min = number.slice(1, number.length)
+  // console.log(min, 'min');
+  for (i = 0; i < number.length; i++) {
+    // ебануцца як
+    let ii = number.slice(0, i).concat(number.slice(i+1, number.length))
+    // console.log(min, 'min');
+    // console.log(ii, 'ii');
+    if (ii > min){
+      min = ii
+    }
+  }
+  return +(min.join(''))
 }
 
 module.exports = {
   deleteDigit
 };
+
+// console.log(module.exports.deleteDigit(1352))
+console.log(module.exports.deleteDigit(1001))
