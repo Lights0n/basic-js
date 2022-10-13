8efc313d8de9cba7b1abb055354391b29c68b081
Lights0n <lyadouskiy@gmail.com>
2022-10-13 19:16:31 +0300

    feat: implement encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // console.log([...str]);
+  let ans = []
+  let counter = 1
+  for (let i = 0; i < str.length; i++) {
+    // console.log(str[i], 'here');
+    if (str[i] == str[i + 1]) {
+      counter += 1
+      continue
+    }
+    if ((str[i] != str[i + 1]) && counter != 1) {
+      // console.log('2222');
+      ans.push(counter)
+      ans.push(str[i])
+      counter = 1
+    } else if ((str[i] != str[i + 1]) && counter == 1) {
+      ans.push((str[i]))
+    }
+    // console.log(counter);
+  }
+
+  console.log(ans);
+  return ans.join('')
 }
 
 module.exports = {
   encodeLine
 };
+// console.log(module.exports.encodeLine('abbcca'));
+// console.log(module.exports.encodeLine('aaaatttt'));
+// a2b2ca
