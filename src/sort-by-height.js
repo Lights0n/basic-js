06008bdfe1ae55230ce2ec7713b45009dc9468c5
Lights0n <lyadouskiy@gmail.com>
2022-10-16 13:46:11 +0300

    feat: what-seasone done

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let indexes = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == -1) {
+
+
+      indexes.push(i)
+      // arr.splice(i, 1)
+      delete arr[i]
+      // console.log(arr, 'delete ', removed);
+
+      //  шо су4ара, думал я не придумаю как тебя вернуть, га ? Сидеть!
+
+
+    }
+  }
+  // console.log(indexes);
+  // console.log(arr);
+  let sorted = arr.sort(function (a, b) {
+
+    return a - b
+  })
+  for (let i = 0; i < indexes.length; i++) {
+    sorted.splice(indexes[i], 0, -1)
+  }
+  // console.log(sorted, 'dorted');
+  sorted.splice(sorted.length - indexes.length, indexes.length)
+  // console.log(sorted, 'Sorted');
+  return sorted;
 }
 
 module.exports = {
   sortByHeight
 };
+// console.log(module.exports.sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getSeason(date) {
+  // let date = new Date(2025, 1, 22, 23, 45, 11, 500)
+  // throw new NotImplementedError('Not implemented');
+
+  let real = new Date(2025, 1, 22, 23, 45, 11, 500)
+  if (arguments[0] == undefined) {
+    console.log(date, '<- Unable ->');
+    return 'Unable to determine the time of year!'
+  }
+  // утки не пройдут
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length != Object.getOwnPropertyNames(real).length) {
+    throw new Error('Invalid date!')
+  }
+  let month = date.getMonth()
+
+  if (month == 0 || month == 1 || month == 11) {
+    return 'winter'
+  }
+  else if (month == 2 || month == 3 || month == 4) {
+    return 'spring'
+  }
+  else if (month == 5 || month == 6 || month == 7) {
+    return 'summer'
+  }
+  else if (month == 8 || month == 9 || month == 10) {
+    return 'fall'
+  }
 }
 
 module.exports = {
   getSeason
 };
+// console.log(module.exports.getSeason());
